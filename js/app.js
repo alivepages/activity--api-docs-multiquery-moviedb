2f467f2e43f3d9c006d26de3157e2cde3299b7a2
Joel <joel.fagundo@gmail.com>
2017-10-19 10:06:59 -0500

    fix endpoint

--- js/app.js
+++ js/app.js
@@ -1,15 +1,15 @@
 var key = 'ad87e3b40153e152dcf276e63868653c';
 var request = superagent
-  .get('https://api.themoviedb.org/3/discover/movie?api_key=' + key + '&sort_by=popularity.desc&page=1')
+  .get('https://api.themoviedb.org/3/movie/top_rated?api_key=' + key)
   .then(function(data) {
-    var movie = data.body.results[0].title;
-    var movie_id = data.body.results[0].id;
+    var movie = data.body.results[1].title;
+    var movie_id = data.body.results[1].id;
     document.querySelector('.movie-title mark').textContent = movie;
     return movie_id;
   })
   .then(function(movie_id){
     var request = superagent
-    .get('http://api.themoviedb.org/3/movie/'+movie_id+'/casts?api_key='+key)
+    .get('https://api.themoviedb.org/3/movie/'+movie_id+'/credits?api_key='+key)
     .then(function(data) {
       var cant = data.body.cast.length;
       document.querySelector('.cast-count mark').textContent = cant;
